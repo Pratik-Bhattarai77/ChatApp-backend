8b3bce5540252561ceb022886cb84a2e35cb0659
Nutan Panta <nutanrajpanta@gmail.com>
2024-07-27 23:04:12 +0545

    Updated route to have index route

--- server.js
+++ server.js
@@ -1,16 +1,16 @@
 //packages import
-import path from "path";
-import express from "express";
-import dotenv from "dotenv";
-import cookieParser from "cookie-parser";
+import path from 'path';
+import express from 'express';
+import dotenv from 'dotenv';
+import cookieParser from 'cookie-parser';
 
 //routes import
-import authRoutes from "./routes/auth.routes.js";
-import messageRoutes from "./routes/message.routes.js";
-import userRoutes from "./routes/user.routes.js";
+import authRoutes from './routes/auth.routes.js';
+import messageRoutes from './routes/message.routes.js';
+import userRoutes from './routes/user.routes.js';
 //database import
-import connectToMongo from "./Data/connecttoMongos.js";
-import { app, server } from "./socket/socket.js";
+import connectToMongo from './Data/connecttoMongos.js';
+import { app, server } from './socket/socket.js';
 
 dotenv.config();
 
@@ -21,19 +21,19 @@ const PORT = process.env.PORT || 5000;
 app.use(express.json()); // for parsing application/json (from req.body)
 app.use(cookieParser());
 
+app.use('/api/auth', authRoutes);
+app.use('/api/messages', messageRoutes);
+app.use('/api/users', userRoutes);
 
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
-app.use("/api/users",userRoutes)
-
-app.use(express.static(path.join(__dirname,"/frontend/dist")));
-
-app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+app.get(['/', BASE_URL], function (req, res) {
+  return res.status(200).json({
+    status: 'success',
+    statusCode: 200,
+    message: 'Greetings from chat app api',
+  });
 });
 
-
-server.listen(PORT,()=> {
-    connectToMongo();
-    console.log(`server running on port ${PORT}`)    
+server.listen(PORT, () => {
+  connectToMongo();
+  console.log(`server running on port ${PORT}`);
 });
